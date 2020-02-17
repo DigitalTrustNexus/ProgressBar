6ae10d6e1cb84c7a6f7da99bae9a4cf4dd53e5f8
EC2 Default User <ec2-user@ip-172-31-37-253.us-east-2.compute.internal>
2020-02-17 23:11:55 +0000

    comvert using https and wss added additonal locations separate location portion into small htmls

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var LISTEN_PORT = 8086;
 var DEFAULT_FILE = "index.html"; 
 
 const WebSocket = require('ws')
-var http = require('http'), 
+var https = require('https'), 
 fs = require('fs'); 
 
+const options = {
+  key: fs.readFileSync('certs/www2.digitaltrustnexus.net.key'),
+  cert: fs.readFileSync('certs/www2.digitaltrustnexus.net.cer'),
+  //ca: [fs.readFileSync('apig-cert.pem')],
+  //ca: [fs.readFileSync('nuc1_cert.pem'), fs.readFileSync('ca.cer'), fs.readFileSync('fullchain.cer')],
+  ca: [fs.readFileSync('certs/apig-cert.pem'), fs.readFileSync('certs/ca.cer'), fs.readFileSync('certs/fullchain.cer')],
+  requestCert: true, 
+  rejectUnauthorized: false,
+  port: LISTEN_PORT
+};
 
 function getExtension(fileName) { 
     var fileNameLength = fileName.length; 
@@ -35,9 +45,14 @@ function getContentType(fileName) {
     return contentType_value; 
 } 
 
-var server  = http.createServer(); 
-server.on('request', 
+var server  = https.createServer(options, (socket) =>{
+  console.log('server connected', 
+  socket.authorized ? 'authorized' : 'unauthorized');
+});
+
+server.on('request',  
     function(request, response){ 
+        console.log(request.socket.getPeerCertificate());
         console.log('Requested Url:' + request.url); 
         var requestedFile = request.url; 
         requestedFile = (requestedFile.substring(requestedFile.length - 1, 1) === '/')?requestedFile + DEFAULT_FILE : requestedFile; 
@@ -59,25 +74,23 @@ server.on('request',
 ); 
 
 server.listen(LISTEN_PORT, LISTEN_IP); 
-const wss = new WebSocket.Server({ port: 8087 })
+
+var httpsServer  = https.createServer(options); 
+httpsServer.listen(LISTEN_PORT+1, LISTEN_IP); 
+
+const ws = new WebSocket.Server({server: httpsServer})
 console.log('Server running at http://' + LISTEN_IP + ':' + LISTEN_PORT); 
 //var clients = [];
 var client;
 
-wss.on('connection', ws => {
+ws.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
     if (message == "init") {
     	client = ws;
     } else {
         client.send(message);
-/*
-      for(var i = 0; i < clients.length; i++) {
-          clients[i].send(message);
-      }
-*/
     }
   })
 })
-
 
